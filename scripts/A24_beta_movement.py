f5bec3827c8d3254f1e7752030530248ce7afb90
jwelzel <52565341+JuliusWelzel@users.noreply.github.com>
2025-07-04 10:02:43 +0200

    Refactor A24_beta_movement.py to load and process EEG info from CSV, merging bad channels, components, and trials

--- scripts/A24_beta_movement.py
+++ scripts/A24_beta_movement.py
@@ -2,9 +2,26 @@ import mne
 from mne.io import read_raw_eeglab
 from mne.viz import plot_compare_evokeds, plot_events
 from pathlib import Path
+import pandas as pd
 
 # Set the directory containing your .set files
 set_files_dir = Path(r'C:\Users\juliu\Desktop\oldenburg\mobile_basketball_eeg\data\clean_ica')  # Change this to your directory
+
+# load .mat file for EEG info
+dir_info = Path(r'C:\Users\juliu\Desktop\oldenburg\mobile_basketball_eeg\data\raw')  # Change this to your directory
+info_file = dir_info.joinpath('Info_EEG.csv')
+
+info_eeg = pd.read_csv(info_file)
+# Find all columns that start with 'Bad_channels_n', 'Bad_components_n', or 'Bad_trials_n'
+bad_channels_cols = [col for col in info_eeg.columns if col.startswith('Bad_channels')]
+bad_components_cols = [col for col in info_eeg.columns if col.startswith('Bad_components')]
+bad_trials_cols = [col for col in info_eeg.columns if col.startswith('Bad_trials')]
+
+# Merge the columns into a single list per row
+info_eeg['Bad_channels_merged'] = info_eeg[bad_channels_cols].apply(lambda row: [item for item in row if pd.notnull(item)], axis=1)
+info_eeg['Bad_components_merged'] = info_eeg[bad_components_cols].apply(lambda row: [item for item in row if pd.notnull(item)], axis=1)
+info_eeg['Bad_trials_merged'] = info_eeg[bad_trials_cols].apply(lambda row: [item for item in row if pd.notnull(item)], axis=1)
+
 
 # List all .set files in the directory
 set_files = list(set_files_dir.glob('*.set'))
